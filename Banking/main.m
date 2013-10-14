12fac150d2630cd82e2861ce1f3e496257ad972c
Christian Høj <cbh@ucn.dk>
2013-10-14 14:57:23 +0200

    Initial Commit

---
+++ Banking/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Banking
+//
+//  Created by Christian Høj on 14/10/13.
+//  Copyright (c) 2013 Christian Høj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
